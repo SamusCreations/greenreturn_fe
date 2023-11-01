be91393df1e61dadf0c6c41afebeadbe4178b642
Luijaxx <luija2000@gmail.com>
2023-10-31 19:53:23 -0600

    materialdetalle y lista terminados

--- src/assets/images.jsx
+++
@@ -1,3 +0,0 @@
-export { default as image1 } from './paper.png'
-export { default as image2 }  from './cardboard.png'
-export { default as image3 }  from './scrap.png'
--- src/components/Movie/ListMaterial.jsx
+++ src/components/Movie/ListMaterial.jsx
@@ -2,19 +2,10 @@ import { useEffect, useState } from "react";
 import MaterialService from "../../services/MaterialService";
 // import Card from "@mui/material/Card";
 // import CardHeader from "@mui/material/CardHeader";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import AccessTime from "@mui/icons-material/AccessTime";
-import Language from "@mui/icons-material/Language";
 import { Link } from "react-router-dom";
-import { Info } from "@mui/icons-material";
 // import {Card, CardFooter, Image, Button} from "@nextui-org/react";
-import {Card, CardHeader, CardBody, CardFooter, Image, Button} from "@nextui-org/react";
-import * as images from "../../assets/images"
-import logo from "../../assets/paper.png";
+import {Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 function getImgUrl(name) {
   return new URL(`${name}`, import.meta.url).href
 }
@@ -54,12 +45,11 @@ export function ListMaterial() {
           <Grid item xs={4} key={item.id_material}>
             <Link to={`/material/${item.id_material}`}> 
             <Card shadow="sm" className=" flex flex-col w-full max-h-auto" isPressable onPress={() => console.log("a")}>
-          <CardBody id="a" className="overflow-visible p-0 ">
+          <CardBody id="a"  style={{ backgroundColor: item.color_value }} className={`overflow-visible p-0 `}>
             <Image
               shadow="sm"
               radius="lg"
               width="100%"
-              id="aa"
               alt={item.title}
               className="flex flex-col justify-center "
               src={ getImgUrl(item.image_url) }
