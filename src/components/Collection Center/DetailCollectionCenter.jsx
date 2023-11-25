2db28b469b1950e14d7f428befcf97ab39d05888
SamusCreations <samirr022004@gmail.com>
2023-11-25 15:50:21 -0600

    Mejoras en diseño de listas, filtro cc por material

--- src/components/Collection Center/DetailCollectionCenter.jsx
+++ src/components/Collection Center/DetailCollectionCenter.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import CCService from "../../services/CollectionCenterService";
 import { Card, CardBody, CardFooter, Image, Spinner } from "@nextui-org/react";
 import { HomeIcon } from "../../assets/Icons";
+import fallback from "../../assets/fallback.png";
 
 export function DetailCollectionCenter() {
   const routeParams = useParams();
@@ -42,8 +43,8 @@ export function DetailCollectionCenter() {
 
   return (
     <div>
-      <div className="font-bold text-4xl py-8">
-        <h1 className="capitalize">Collection Center Detail</h1>
+      <div className="font-bold text-4xl py-8 mx-2">
+        <h1 className="uppercase">Collection Center Detail</h1>
       </div>
       <div>
         {data && (
@@ -52,7 +53,7 @@ export function DetailCollectionCenter() {
               <HomeIcon
                 className="text-primary mx-auto animate-appearance-in"
                 fill="currentColor"
-                size={300}
+                size={250}
               />
             </div>
             <div className="flex-1 items-center justify-center px-4">
@@ -65,49 +66,69 @@ export function DetailCollectionCenter() {
                   {`${data.address}, ${data.district.name}, ${data.canton.name}, ${data.province.name}`}
                 </p>
                 <p className="py-2 text-lg font-medium">
-                <span className="font-bold"> Telephone: </span>
+                  <span className="font-bold"> Telephone: </span>
                   {data.telephone}
                 </p>
                 <p className="py-2 text-lg font-medium">
-                <span className="font-bold"> Schedule: </span>
+                  <span className="font-bold"> Schedule: </span>
                   {data.schedule}
                 </p>
                 <p className="py-2 text-lg font-medium">
-                <span className="font-bold"> Administrator: </span>
+                  <span className="font-bold"> Administrator: </span>
                   {data.administrator.name} {data.administrator.surname}
                 </p>
               </div>
             </div>
           </div>
         )}
-        <div className="sm:gap-2 grid grid-cols-1 sm:grid-cols-4 py-4">
-          {data.materials.map((item, index) => (
-            <Link
-              to={`/material/${item.id_material}`}
-              key={index}
-              className="p-2 sm:p-0"
-            >
-              <Card shadow="sm" key={index} isPressable className="w-full animate-appearance-in">
-                <CardBody
-                  className="overflow-visible p-0 rounded-xl w-full"
-                  style={{ backgroundColor: item.color_value }}
+        <div>
+          <div className="font-bold text-4xl py-6 mx-2">
+            <h1 className="uppercase">Available Materials</h1>
+          </div>
+          {!data.materials && (
+            <div className="text-center ">
+              <p>Not available yet</p>
+            </div>
+          )}
+          <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-2">
+            {data.materials &&
+              data.materials.map((item, index) => (
+                <Link
+                  to={`/material/${item.id_material}`}
+                  key={index}
+                  className="p-2 sm:p-0 max-w-[300px] mx-auto"
                 >
-                  <Image
+                  <Card
                     shadow="sm"
-                    radius="lg"
-                    width="100%"
-                    alt={item.name}
-                    className="w-full object-cover"
-                    src={item.image_url}
-                  />
-                </CardBody>
-                <CardFooter className="text-small justify-between">
-                  <b>{item.name}</b>
-                  <p className="text-default-500">{item.unit_cost} Ecocoins</p>
-                </CardFooter>
-              </Card>
-            </Link>
-          ))}
+                    key={index}
+                    isPressable
+                    className="w-full animate-appearance-in"
+                  >
+                    <CardBody
+                      className="overflow-visible p-0 rounded-xl"
+                      style={{ backgroundColor: item.color_value }}
+                    >
+                      <Image
+                        shadow="sm"
+                        radius="lg"
+                        alt={item.name}
+                        src={item.image_url}
+                        className="mx-auto w-full object-cover"
+                        fallbackSrc={fallback}
+                        isBlurred
+                        isZoomed
+                      />
+                    </CardBody>
+                    <CardFooter className="text-small justify-between">
+                      <b>{item.name}</b>
+                      <p className="text-default-500">
+                        {item.unit_cost} Ecocoins
+                      </p>
+                    </CardFooter>
+                  </Card>
+                </Link>
+              ))}
+          </div>
         </div>
       </div>
     </div>
